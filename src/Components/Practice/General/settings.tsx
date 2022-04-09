078e4b1e8ac2c2e7ec15201c427c24279246c315
Sylvan <sudsysquid@gmail.com>
2022-04-09 10:37:51 -0400

    settings page version one for quizzes

--- src/Components/Practice/General/settings.tsx
+++ src/Components/Practice/General/settings.tsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const PracticeSettings: React.FC = () => {
+  const [AnswerWithName, setAnswerWithName] = useState(true);
+
   return (
-    <div className="flex flex-col justify-end h-full">
-      <h1 className="mt-3 overflow-x-auto text-xl font-bold text-center">
-        Your Answer
-      </h1>
+    <div className="flex flex-col h-full">
+      <div className="mt-10">
+        <Setting
+          name={"Quiz Names"}
+          current={AnswerWithName}
+          setter={setAnswerWithName}
+        ></Setting>
+      </div>
+    </div>
+  );
+};
+
+interface SettingProps {
+  name: string;
+  current: boolean;
+  setter: Function;
+}
+
+const Setting: React.FC<SettingProps> = (props) => {
+  return (
+    <div className="bg-standard p-3">
+      <h1> {props.name}</h1>
+
+      <button
+        className="button-standard"
+        onClick={() => props.setter(!props.current)}
+      >
+        {String(props.current)}
+      </button>
     </div>
   );
 };
