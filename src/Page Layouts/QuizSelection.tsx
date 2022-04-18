f54f5cdd9f8d67f615b82101ad3e4850a591d778
Sylvan <sylvanfranklin@icloud.com>
2022-04-18 14:04:11 -0400

    re-added hover animation on quiz selection

--- src/Page Layouts/QuizSelection.tsx
+++ src/Page Layouts/QuizSelection.tsx
@@ -37,7 +37,7 @@ const Quiz: FunctionComponent<QuizProps> = (props) => {
   return (
     <div
       onClick={() => navigator(props.path)}
-      className="bg-standard m-2 mx-auto flex select-none flex-row items-center rounded-md transition-all duration-100 ease-in-out hover:bg-opacity-50"
+      className="bg-standard m-2 mx-auto flex select-none flex-row items-center rounded-md transition-all duration-100 ease-in-out hover:brightness-90 dark:hover:brightness-150"
     >
       <h1 className="bg-opacity-5 p-5 text-center text-xl text-light-text dark:text-dark-text">
         {props.name}
@@ -56,7 +56,7 @@ const QuizFolder: React.FC<QuizFolderProps> = (props) => {
     <div className="">
       <div
         onClick={() => setFolderOpen(!folderOpen)}
-        className="bg-standard m-2 mx-auto flex select-none flex-row items-center rounded-r-md transition-all duration-100 ease-in-out hover:bg-opacity-50"
+        className="bg-standard m-2 mx-auto flex select-none flex-row items-center rounded-md transition-all duration-100 ease-in-out hover:brightness-90 dark:hover:brightness-150"
       >
         <div className="flex flex-row bg-opacity-5 p-5 text-center text-xl text-light-text dark:text-dark-text">
           <div className="my-auto mr-4">
