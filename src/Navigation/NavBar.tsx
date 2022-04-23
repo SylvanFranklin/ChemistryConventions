309e4cec8ef7c0501b5de4440410749c65721241
Sylvan <sylvanfranklin@icloud.com>
2022-04-23 14:11:13 -0400

    visual update to nav bar

--- src/Navigation/NavBar.tsx
+++ src/Navigation/NavBar.tsx
@@ -4,7 +4,7 @@ import NavItem from "./NavItem";
 
 const NavBar: FC = () => {
   return (
-    <div className="mb-10 flex h-20 flex-row justify-end text-light-text dark:border-dark-gray dark:text-dark-text mr-6">
+    <div className="mb-10 mr-6 flex h-20 flex-row justify-end text-light-text dark:border-dark-gray dark:text-dark-text">
       <NavItem location={"/"}>
         <h2>Table</h2>
       </NavItem>
@@ -16,7 +16,6 @@ const NavBar: FC = () => {
       </NavItem>
       <NavItem>
         <DarkModeSwitch />
-
       </NavItem>
     </div>
   );
