15d1a5a134f343a90eff8b423c91acbf76530115
Sylvan <sylvanfranklin@icloud.com>
2022-04-25 14:28:19 -0400

    made nav items closer to the center

--- src/Navigation/NavBar.tsx
+++ src/Navigation/NavBar.tsx
@@ -1,17 +1,17 @@
 import React, { FC } from "react";
 import { DarkModeSwitch } from "./DarkModeSwitch";
 import NavItem from "./NavItem";
-import { TableButton } from './TableButton';
+import { TableButton } from "./TableButton";
 
 const NavBar: FC = () => {
   return (
-    <div className="mx-14 mb-10 flex h-20 flex-row justify-end text-light-text dark:border-dark-gray dark:text-dark-text">
-<TableButton     />
+    <div className="mx-40 mb-10 flex h-20 flex-row justify-end text-light-text dark:border-dark-gray dark:text-dark-text">
+      <TableButton />
       <div className="w-full"></div>
       <NavItem location={"/practice"}>
         <h2>Practice</h2>
       </NavItem>
-        <DarkModeSwitch />
+      <DarkModeSwitch />
     </div>
   );
 };
