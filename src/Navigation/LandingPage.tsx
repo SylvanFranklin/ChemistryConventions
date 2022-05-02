b72262687be3e27da3707602b0d56a6975f2ad18
Sylvan <sylvanfranklin@icloud.com>
2022-05-01 20:36:30 -0400

    added content to landing page

--- src/Navigation/LandingPage.tsx
+++ src/Navigation/LandingPage.tsx
@@ -1,10 +1,30 @@
-import DefaultLayout from "../Page Layouts/Default"
+import DefaultLayout from "../Page Layouts/Default";
 
+const Landing: React.FC = () => {
+  return (
+    <DefaultLayout title="About the project">
+      <p className="text-standard bg-standard mx-auto w-1/2 rounded-lg p-4 text-lg">
+        "Chemistry Conventions" is a website designed for practicing the many
+        naming and formula conventions of chemistry. All questions are
+        algorithmically generated, which means there's an endless supply.
+        <p className="mt-10">
+          *Currently under development, so you're likely to encounter a few bugs
+          and missing features.
+        </p>
+      </p>
 
-const Landing:React.FC = () => {
+      <p className="text-standard bg-standard mx-auto mt-10 w-1/2 rounded-lg p-4 text-lg">
+        For anyone looking to contribute the site is on {""}
+        <a
+          href="https://github.com/SylvanFranklin/ChemistryConventions"
+          className="text-blue-200 underline"
+        >
+          GitHub
+        </a>
+        {""} with installation instructions.
+      </p>
+    </DefaultLayout>
+  );
+};
 
-return (<DefaultLayout title="home"></DefaultLayout>)
-}
-
-
-export default Landing
+export default Landing;
