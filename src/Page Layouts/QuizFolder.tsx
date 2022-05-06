da73170c217890d43f7b358a308d425b0f455a1b
Sylvan <sylvanfranklin@icloud.com>
2022-05-06 19:43:26 -0400

    updated folder props to take children

--- src/Page Layouts/QuizFolder.tsx
+++ src/Page Layouts/QuizFolder.tsx
@@ -1,25 +1,23 @@
-import { motion } from "framer-motion";
-import React, { useState } from "react";
+import React, { ReactNode, useState } from "react";
 import { BiDownArrow, BiRightArrow } from "react-icons/bi";
-import { Quiz, QuizProps } from "./Quiz";
 
 export interface QuizFolderProps {
-  content: QuizProps[];
   name: string;
+  children: ReactNode;
 }
 
 export const QuizFolder: React.FC<QuizFolderProps> = (props) => {
   const [folderOpen, setFolderOpen] = useState(false);
 
   return (
-    <div className="">
-      <motion.div
+    <>
+      <div
         onClick={() => {
           setFolderOpen(!folderOpen);
         }}
-        className="bg-standard m-2 mx-auto flex select-none flex-row items-center rounded-md transition-all duration-100 ease-in-out hover:brightness-90 dark:hover:brightness-150"
+        className="bg-standard m-2 mx-auto flex w-full select-none flex-row items-center rounded-md transition-all duration-100 ease-in-out hover:brightness-90 dark:hover:brightness-150"
       >
-        <div className="flex flex-row bg-opacity-5 p-5 text-center text-xl text-light-text dark:text-dark-text">
+        <div className="flex flex-row bg-opacity-5 p-5 text-center text-xl text-light-text dark:text-dark-text ">
           <div className="my-auto mr-4">
             {folderOpen ? (
               <BiDownArrow className="my-auto" />
@@ -29,19 +27,8 @@ export const QuizFolder: React.FC<QuizFolderProps> = (props) => {
           </div>
           {props.name}
         </div>
-      </motion.div>
-
-      {folderOpen ? (
-        <div className="pl-10">
-          {props.content.map((quiz) => (
-            <Quiz
-              name={quiz.name}
-              path={`${props.name.toLowerCase()}/${quiz.path}`}
-              key={quiz.name}
-            />
-          ))}
-        </div>
-      ) : null}
-    </div>
+      </div>
+      {folderOpen && <div className="ml-auto w-4/5">{props.children}</div>}
+    </>
   );
 };
