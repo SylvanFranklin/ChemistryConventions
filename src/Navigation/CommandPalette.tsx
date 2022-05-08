8fba31ce6df6c87ee78d633fb1b754f05ecb4f62
Sylvan <sylvanfranklin@icloud.com>
2022-05-08 10:56:52 -0400

    added links for all palette elements

--- src/Navigation/CommandPalette.tsx
+++ src/Navigation/CommandPalette.tsx
@@ -1,6 +1,24 @@
-import { Combobox, Dialog } from "@headlessui/react";
-import { useEffect, useState } from "react";
-import { elements } from "./ElementValues";
+import { ReactNode } from "react";
+import { useNavigate } from "react-router-dom";
+import elements from "../ElementalJson/table.json";
+
+interface PaletteItemProps {
+  title: string;
+  action: Function;
+  description: string;
+}
+
+const PaletteItem: React.FC<PaletteItemProps> = (props) => {
+  return (
+    <li
+      onClick={() => props.action()}
+      className="text-standard bg-standard flex flex-row p-4 text-lg hover:brightness-90"
+    >
+      {props.title}
+      <h3 className="ml-auto mr-4 text-gray-400">{props.description}</h3>
+    </li>
+  );
+};
 
 interface CommandPaletteProps {
   isOpen: boolean;
@@ -8,6 +26,23 @@ interface CommandPaletteProps {
 }
 
 export const CommandPalette: React.FC<CommandPaletteProps> = (props) => {
+  let PaletteItems: ReactNode[] = [];
+  let navigate = useNavigate();
+
+  for (const element of Object.values(elements)) {
+    PaletteItems.push(
+      <PaletteItem
+        title={element.name}
+        action={() => {
+          props.setIsOpen(false);
+          navigate(`/element/${element.name}`);
+        }}
+        description={"element"}
+        key={element.name}
+      />
+    );
+  }
+
   return (
     <>
       {props.isOpen && (
@@ -17,16 +52,17 @@ export const CommandPalette: React.FC<CommandPaletteProps> = (props) => {
         >
           <div
             onClick={(e) => e.stopPropagation()}
-            className="bg-standard fixed top-0 right-0 left-0 z-50 mx-auto mt-32 h-1/2 w-1/2 rounded-lg"
+            className="bg-standard fixed top-0 right-0 left-0 z-50 mx-auto mt-32 h-1/2 w-1/2 overflow-scroll rounded-lg"
           >
-            <form className="">
+            <form className="" onSubmit={(e) => e.preventDefault()}>
               <input
                 type="text"
-                className="text-standard bg-standard block w-full rounded-t-lg p-4 text-lg focus:outline-none"
-                placeholder="Search for element, compounds, or quizzes..."
+                className="text-standard bg-standard fixed block w-1/2 rounded-t-lg border-b-2 p-4 text-lg focus:outline-none dark:border-slate-600"
+                placeholder="Search for elements, compounds, or quizzes..."
                 autoFocus
                 autoComplete="false"
               ></input>
+              <ol>{PaletteItems}</ol>
             </form>
           </div>
         </div>
