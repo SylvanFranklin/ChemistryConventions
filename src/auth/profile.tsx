32ff8e3db48888ba0c71cdad7d7e88bcdac85777
Sylvan <sylvanfranklin@icloud.com>
2022-05-23 07:59:07 -0400

    changs to style

--- src/auth/profile.tsx
+++ src/auth/profile.tsx
@@ -8,6 +8,7 @@ import DefaultLayout from "../Page Layouts/Default";
 import { LogIn } from "./Login";
 import { SignUp } from "./SignUp";
 
+
 const svg = createAvatar(style, {
   seed: "custom-seed",
   // ... and other options
@@ -19,30 +20,34 @@ export const Profile: React.FC = () => {
   return (
     <DefaultLayout>
       <div className="mx-auto h-full w-1/2 rounded-md p-6">
-        <>
-          {user ? (
-            // <div onClick={() => signOut(auth)}>
-            //   some stats {user.displayName}
-            // </div>
-            <div className="bg-standard flex flex-row rounded-md p-6">
+        {user ? (
+          // <div onClick={() => signOut(auth)}>
+          //   some stats {user.displayName}
+          // </div>
+          <div className="bg-standard flex h-max flex-row rounded-md p-6">
+            <div className="flex h-full w-full">
               <div
                 className="h-1/6 w-1/6 overflow-clip rounded-full border-4"
                 dangerouslySetInnerHTML={{ __html: svg }}
+                onClick={() => auth.signOut()}
               ></div>
+              <h2 className="text-standard mt-4">
+                {user.displayName ? user.displayName : "unnamed account"}
+              </h2>
             </div>
-          ) : (
-            !loading && ( // otherwise render a sign in page
-              <div className="flex flex-row">
-                <div className="mr-16 w-1/2 ">
-                  <SignUp />
-                </div>
-                <div className="ml-16 w-1/2">
-                  <LogIn />
-                </div>
+          </div>
+        ) : (
+          !loading && ( // otherwise render a sign in page
+            <div className="flex flex-row">
+              <div className="mr-16 w-1/2 ">
+                <SignUp />
               </div>
-            )
-          )}
-        </>
+              <div className="ml-16 w-1/2">
+                <LogIn />
+              </div>
+            </div>
+          )
+        )}
       </div>
     </DefaultLayout>
   );
